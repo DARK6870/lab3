60f20c2149f2d1200b43e3113deb35626deb2a05
DARK6870 <tsymbalvlad.6870@gmail.com>
2024-01-30 20:50:08 +0200

    init

---
+++ Vlad_bal3/Vlad_bal3/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vlad_bal3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void openRegForm_Click(object sender, RoutedEventArgs e)
+        {
+            Registration window = new Registration();
+            window.Show();
+            this.Close();
+        }
+
+        private void clear_btn_Click(object sender, RoutedEventArgs e)
+        {
+            username_tb.Text = string.Empty;
+            password_tb.Password = string.Empty;
+        }
+    }
+}
---
+++ Vlad_bal3/Vlad_bal3/Registration.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vlad_bal3
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void openLoginForm_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow windows = new MainWindow();
+            windows.Show();
+            this.Close();
+        }
+
+        private void clear_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
